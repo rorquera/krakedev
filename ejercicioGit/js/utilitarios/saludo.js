c2bfa04fb1723a218bb40a9b5be7216f36ff97d1
rorquera <arckanhell1@gmail.com>
2024-08-07 18:41:38 -0500

    agregacion de cajas de texto para edad y estatura para probar los convertidores a entero y flotante

--- ejercicioGit/js/utilitarios/saludo.js
+++ ejercicioGit/js/utilitarios/saludo.js
@@ -3,6 +3,10 @@ const saludar = () => {
   let nombre = recuperarTexto('txtNombre');
   //Recupera apellido
   let apellido = recuperarTexto('txtApellido');
+  //Recupera edad
+  let edad = recuperarInt('txtEdad');
+  //Recupera estatura
+  let estatura = recuperarFloat('txtEstatura');
 };
 
 const recuperarTexto = (idComponente) => {
@@ -11,4 +15,16 @@ const recuperarTexto = (idComponente) => {
   componente = document.getElementById(idComponente);
   valorIngresado = componente.value;
   return valorIngresado;
+};
+
+const recuperarInt = (idComponente) => {
+  let valorCaja = recuperarTexto(idComponente);
+  let valorEntero = parseInt(valorCaja);
+  return valorEntero;
+};
+
+const recuperarFloat = (idComponente) => {
+  let valorCaja = recuperarTexto(idComponente);
+  let valorFlotante = parseFloat(valorCaja);
+  return valorFlotante;
 };
