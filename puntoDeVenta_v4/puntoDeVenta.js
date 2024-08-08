49dbc154b1325775974de9bb471cc27c0a9531fa
rorquera <arckanhell1@gmail.com>
2024-08-08 12:56:57 -0500

    Implementacion de resumen y pruebas generales

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -88,6 +88,18 @@ calcularValorTotal = function () {
        */
 
   //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
+  nombreProducto = recuperarTexto('txtProducto');
+  mostrarTexto(
+    'lblResumen',
+    'Valor a pagar por ' +
+      cantidad +
+      ' ' +
+      nombreProducto +
+      ' con el ' +
+      porcentajeDescuento +
+      '% de descuento: ' +
+      valorTotal
+  );
   /*
         Ejemplo: 
             Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
