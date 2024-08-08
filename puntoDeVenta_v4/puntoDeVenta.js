2562191712d5eb522da3a2c31f170d60dead4892
rorquera <arckanhell1@gmail.com>
2024-08-08 12:35:57 -0500

    Pruebas para la funcion calcularIVA

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -50,7 +50,9 @@ calcularValorTotal = function () {
      */
   //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
   // El IVA debe calcularse sobre el valor del subtotal menos el descuento
+  valorIVA = calcularIVA(valorSubtotal - valorDescuento);
   //9. Mostrar el resultado en el componente lblValorIVA
+  mostrarTexto('lblValorIVA', valorIVA);
   /*
             Caso de prueba: 
                 - cantidad: 10 
