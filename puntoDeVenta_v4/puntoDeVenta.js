b72e1ce938ef57d8d84f9df31be0fc504a148de4
rorquera <arckanhell1@gmail.com>
2024-08-08 13:17:06 -0500

    Implementacion de la funcion limpiar, pruebas generales

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -112,6 +112,15 @@ limpiar = function () {
         Dejar todos los textos de los montos con el valor 0.0
         Si funciona, hacer un commit
      */
+  mostrarTextoEnCaja('txtProducto', '');
+  mostrarTextoEnCaja('txtCantidad', '0.0');
+  mostrarTextoEnCaja('txtPrecio', '0.0');
+  mostrarTextoEnCaja('txtPorcentajeDescuento', '0.0');
+  mostrarTexto('lblSubtotal', '0.0');
+  mostrarTexto('lblDescuento', '0.0');
+  mostrarTexto('lblValorIVA', '0.0');
+  mostrarTexto('lblTotal', '0.0');
+  mostrarTexto('lblResumen', '');
 };
 /* SI TODO FUNCIONA, HACER UN PUSH */
 
