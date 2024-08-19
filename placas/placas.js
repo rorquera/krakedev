3544e186f85dd63ee159f5a091c8378f922d5604
rorquera <arckanhell1@gmail.com>
2024-08-19 15:30:39 -0500

    Obtención de la provincia y pruebas

--- placas/placas.js
+++ placas/placas.js
@@ -1,9 +1,19 @@
 const validarPlaca = () => {
   let erroresEstructura;
-  erroresEstructura = validarEstructura(recuperarTexto('txtPlaca'));
+  let placa = recuperarTexto('txtPlaca');
+  let provincia;
+  erroresEstructura = validarEstructura(placa);
   if (erroresEstructura === null) {
     mostrarTexto('lblResultadoPlaca', 'ESTRUCTURA VÁLIDA');
     mostrarTexto('lblValidacion', '');
+    provincia = obtenerProvincia(placa);
+    if (provincia != null && provincia != undefined) {
+      mostrarTexto('lblResPlaca', placa);
+      mostrarTexto('lblResProvincia', provincia);
+    } else {
+      mostrarTexto('lblResPlaca', '');
+      mostrarTexto('lblResProvincia', 'PROVINCIA INCORRECTA');
+    }
   } else {
     mostrarTexto('lblResultadoPlaca', 'ESTRUCTURA INCORRECTA');
     mostrarTexto('lblValidacion', erroresEstructura);
--- placas/serviciosPlacas.js
+++ placas/serviciosPlacas.js
@@ -4,28 +4,52 @@ const validarEstructura = (placa) => {
     error = 'La placa debe tener entre 7 y 8 caracteres. \n';
   }
   if (!esMayuscula(placa.charAt(0))) {
-    error = formatearError(error, 'La primera letra de la placa debe ser mayúscula.');
+    error = formatearError(
+      error,
+      'La primera letra de la placa debe ser mayúscula.'
+    );
   }
   if (!esMayuscula(placa.charAt(1))) {
-    error = formatearError(error, 'La segunda letra de la placa debe ser mayúscula.');
+    error = formatearError(
+      error,
+      'La segunda letra de la placa debe ser mayúscula.'
+    );
   }
   if (!esMayuscula(placa.charAt(2))) {
-    error = formatearError(error, 'La tercera letra de la placa debe ser mayúscula.');
+    error = formatearError(
+      error,
+      'La tercera letra de la placa debe ser mayúscula.'
+    );
   }
   if (!esGuion(placa.charAt(3))) {
-    error = formatearError(error, 'La cuarta letra de la placa debe ser un guión.');
+    error = formatearError(
+      error,
+      'La cuarta letra de la placa debe ser un guión.'
+    );
   }
   if (!esDigito(placa.charAt(4))) {
-    error = formatearError(error, 'La quinta letra de la placa debe ser un dígito.');
+    error = formatearError(
+      error,
+      'La quinta letra de la placa debe ser un dígito.'
+    );
   }
   if (!esDigito(placa.charAt(5))) {
-    error = formatearError(error, 'La sexta letra de la placa debe ser un dígito.');
+    error = formatearError(
+      error,
+      'La sexta letra de la placa debe ser un dígito.'
+    );
   }
   if (!esDigito(placa.charAt(6))) {
-    error = formatearError(error, 'La séptima letra de la placa debe ser un dígito.');
+    error = formatearError(
+      error,
+      'La séptima letra de la placa debe ser un dígito.'
+    );
   }
   if (placa.length == 8 && !esDigito(placa.charAt(7))) {
-    error = formatearError(error, 'La octava letra de la placa debe ser un dígito.');
+    error = formatearError(
+      error,
+      'La octava letra de la placa debe ser un dígito.'
+    );
   }
 
   return error;
@@ -38,4 +62,34 @@ const formatearError = (error, leyenda) => {
     error += leyenda + '\n';
   }
   return error;
+};
+
+const obtenerProvincia = (placa) => {
+  let nombreProvincia = null;
+  switch (placa.charAt(0)) {
+      case 'A':nombreProvincia = 'Azuay'; break;
+      case 'B':nombreProvincia = 'Bolívar'; break;
+      case 'U':nombreProvincia = 'Cañar'; break;
+      case 'C':nombreProvincia = 'Carchi'; break;
+      case 'X':nombreProvincia = 'Cotopaxi'; break;
+      case 'H':nombreProvincia = 'Chimborazo'; break;
+      case 'O':nombreProvincia = 'El Oro'; break;
+      case 'E':nombreProvincia = 'Esmeraldas'; break;
+      case 'W':nombreProvincia = 'Galápagos'; break;
+      case 'G':nombreProvincia = 'Guayas'; break;
+      case 'I':nombreProvincia = 'Imbabura'; break;
+      case 'L':nombreProvincia = 'Loja'; break;
+      case 'R':nombreProvincia = 'Los Ríos'; break;
+      case 'M':nombreProvincia = 'Manabí'; break;
+      case 'V':nombreProvincia = 'Morona Santiago'; break;
+      case 'N':nombreProvincia = 'Napo'; break;
+      case 'S':nombreProvincia = 'Pastaza'; break;
+      case 'P':nombreProvincia = 'Pichincha'; break;
+      case 'K':nombreProvincia = 'Sucumbíos'; break;
+      case 'Q':nombreProvincia = 'Orellana'; break;
+      case 'T':nombreProvincia = 'Tungurahua'; break;
+      case 'Z':nombreProvincia = 'Zamora Chinchipe'; break;
+      case 'Y':nombreProvincia = 'Santa Elena'; break;
+  }
+  return nombreProvincia;
 };
